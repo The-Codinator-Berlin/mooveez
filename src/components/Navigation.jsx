6e19488b4c4206dcb2a9ef9eefe68a47e7307fd4
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-16 18:15:35 +0200

    Removed spaces that were not needed for links

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,6 +3,11 @@ import { Link } from "react-router-dom";
 import "../css/Navigation.css";
 
 function Navigation() {
+
+
+
+
+  
   return (
     <>
       <nav className="navigation-box border-b	border-blue-700 h-10 bg-blue-950 p-2">
@@ -12,21 +17,20 @@ function Navigation() {
         >
           Home
         </Link>
-        &nbsp;&nbsp;&nbsp;&nbsp;
+       
         <Link
           to="/mooveez"
           className="navigation-links bg-neutral-900 rounded text-neutral-50 hover:active:bg-green-600 hover:bg-slate-400"
         >
           Mooveez
         </Link>
-        &nbsp;&nbsp;&nbsp;&nbsp;
         <Link
           to="/about"
           className="navigation-links bg-neutral-900 rounded text-neutral-50 hover:active:bg-green-600 hover:bg-slate-400"
         >
           About
         </Link>
-        &nbsp;&nbsp;&nbsp;&nbsp;
+       
         <Link
           to="/login/register"
           className="bg-red-500	rounded text-neutral-50 navigation-links hover:active:bg-green-600 hover:bg-slate-400"
