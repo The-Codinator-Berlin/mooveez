259424c07bde41e9399c7597ecfbaa001e4c18e1
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-16 14:42:22 +0200

    Paths created for views

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import './views/Page404'
+import Home from "./views/Home"
+import Mooveez from './views/Mooveez';
+import About from './views/About';
+import Page404 from './views/Page404';
+import Navigation from './components/Navigation';
+import LoginRegister from './views/LoginRegister';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navigation/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/mooveez" element={<Mooveez />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<LoginRegister />} />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+
+
     </div>
   );
 }
