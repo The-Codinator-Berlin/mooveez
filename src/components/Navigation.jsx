433158df4edc54aef2ff9f8fcfe0d4f5fa21d155
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-16 14:01:19 +0200

    Navigation compnent added

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import '../css/Navigation.css';
+
+
+function Navigation() {
+  return (
+    <>
+        <nav className="navigation-box border-b	border-blue-700 h-10 bg-blue-950 p-2">
+          <Link className="navigation-links bg-neutral-900 rounded text-neutral-50">Home</Link>&nbsp;&nbsp;&nbsp;&nbsp;
+          <Link className="navigation-links bg-neutral-900 rounded text-neutral-50">Mooveez</Link>&nbsp;&nbsp;&nbsp;&nbsp;
+          <Link className="navigation-links bg-neutral-900 rounded text-neutral-50">About</Link>&nbsp;&nbsp;&nbsp;&nbsp;
+          <button className="bg-red-500	rounded text-neutral-50 navigation-links">Login</button>
+        </nav>
+        
+    </>
+  )
+}
+
+export default Navigation;
