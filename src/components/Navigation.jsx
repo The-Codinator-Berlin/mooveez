0d0f94d9d4b63d9a3db01c5a7c4323257e8d1e92
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-16 16:50:52 +0200

    Links working to navigate /hover:active added

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,10 +7,10 @@ function Navigation() {
   return (
     <>
         <nav className="navigation-box border-b	border-blue-700 h-10 bg-blue-950 p-2">
-          <Link className="navigation-links bg-neutral-900 rounded text-neutral-50">Home</Link>&nbsp;&nbsp;&nbsp;&nbsp;
-          <Link className="navigation-links bg-neutral-900 rounded text-neutral-50">Mooveez</Link>&nbsp;&nbsp;&nbsp;&nbsp;
-          <Link className="navigation-links bg-neutral-900 rounded text-neutral-50">About</Link>&nbsp;&nbsp;&nbsp;&nbsp;
-          <Link className="bg-red-500	rounded text-neutral-50 navigation-links">Login</Link>
+          <Link to="/" className="navigation-links bg-neutral-900 rounded text-neutral-50 hover:active:bg-green-600 hover:bg-slate-400">Home</Link>&nbsp;&nbsp;&nbsp;&nbsp;
+          <Link to="/mooveez" className="navigation-links bg-neutral-900 rounded text-neutral-50 hover:active:bg-green-600 hover:bg-slate-400">Mooveez</Link>&nbsp;&nbsp;&nbsp;&nbsp;
+          <Link to="/about" className="navigation-links bg-neutral-900 rounded text-neutral-50 hover:active:bg-green-600 hover:bg-slate-400">About</Link>&nbsp;&nbsp;&nbsp;&nbsp;
+          <Link to="/login/register" className="bg-red-500	rounded text-neutral-50 navigation-links hover:active:bg-green-600 hover:bg-slate-400">Login</Link>
         </nav>
       
     </>
