f91530e685ae58734d80118d31d9eece6f9d6ea2
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-22 17:33:22 +0200

    Styling added to search

--- src/views/Mooveez.jsx
+++ src/views/Mooveez.jsx
@@ -27,7 +27,7 @@ function Mooveez() {
     <div className="homeDiv bg-neutral-900 text-neutral-50">
       <HeadingPopup />
       <div>
-        <input className="text-neutral-900 rounded mb-1 mt-3 w-80" type="search" placeholder="Search the latest mooveez..."></input>
+        <input className="text-neutral-900 rounded mb-1 mt-3 w-80 p-1 border-2 border-red-600" type="search" placeholder="Search the latest mooveez..."></input>
       </div>
       <div className="grid grid-cols-2 gap-6 pt-1 m-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mooveeCardGrid">
         {mooveez.map((moovee) => (
