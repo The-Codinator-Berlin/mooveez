7561c45ccbb0876a1466cb34bb39ad382a68225b
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-22 16:40:30 +0200

    Added grid and imported MooveeCard component

--- src/views/Mooveez.jsx
+++ src/views/Mooveez.jsx
@@ -1,11 +1,13 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "../css/Mooveez.css";
 import HeadingPopup from "../components/HeadingPopup";
+import MoveeCard from "../components/MoveeCard";
 
 function Mooveez() {
   const [mooveez, setMooveez] = useState([]);
+
   const getMooveez = async () => {
-    let url = `https://api.themoviedb.org/3/discover/movie?api_key=6b4723f60651331047b0a32019781271`;
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=6b4723f60651331047b0a32019781271`;
 
     try {
       const response = await fetch(url);
@@ -24,7 +26,11 @@ function Mooveez() {
   return (
     <div className="homeDiv bg-neutral-900 text-neutral-50">
       <HeadingPopup />
-      <div className="container mx-auto"></div>
+      <div className="grid grid-cols-2 gap-6 pt-1 m-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mooveeCardGrid">
+        {mooveez.map((moovee) => (
+          <MoveeCard key={moovee.id} moovee={moovee} />
+        ))}
+      </div>
     </div>
   );
 }
