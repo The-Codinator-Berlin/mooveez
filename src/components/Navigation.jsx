6f6c64072ef99386455076f68828fad40d1c8dd1
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-23 23:04:09 +0200

    Added user name to nav when logged in

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -28,7 +28,18 @@ function Navigation() {
           About
         </Link>
 
-        { user ? <><Link to="/favourites" className="text-neutral-50 border-blue-700 rounded bg-green-600 hover:active:bg-red-500 hover:bg-slate-400 favouritesButton">Favourites</Link><Link onClick={logout}to="/" className="text-neutral-50 border-blue-700 rounded bg-red-500 hover:active:bg-red-500 hover:bg-slate-400 logOutButton">Log out  </Link></> : 
+        {/* <span> */}
+          {/* {" "} */}
+          {/* {user ? (
+            <h5 className="text-green-500">{user.name} is logged in! </h5>
+          ) : (
+            <h2 className=" py-4 text-blue-600">
+              {" "}
+            </h2>
+          )} */}
+        {/* </span> */}
+
+        { user ? <><h5 className="text-green-500">{user.name} is logged in! </h5><Link to="/favourites" className="text-neutral-50 border-blue-700 rounded bg-green-600 hover:active:bg-red-500 hover:bg-slate-400 favouritesButton">Favourites</Link><Link onClick={logout}to="/" className="text-neutral-50 border-blue-700 rounded bg-red-500 hover:active:bg-red-500 hover:bg-slate-400 logOutButton">Log out  </Link></> : 
         
         <Link to="/login/register" className=" bg-blue-700	rounded text-neutral-50 navigation-link-login hover:active:bg-green-600 hover:bg-slate-400 loginButton border-blue-700 ">Login/Register</Link> }
        
