adede0e82b6e5f3d535c8ca2536541d4f9eeb329
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-23 12:14:09 +0200

    Moved state var and fetch to context

--- src/views/Mooveez.jsx
+++ src/views/Mooveez.jsx
@@ -1,24 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import "../css/Mooveez.css";
 import HeadingPopup from "../components/HeadingPopup";
 import MoveeCard from "../components/MooveeCard";
+import { MooveezContext } from "../context/mooveezContext";
 
 function Mooveez() {
-  const [mooveez, setMooveez] = useState([]);
-
-  const getMooveez = async () => {
-    const url = `https://api.themoviedb.org/3/discover/movie?api_key=6b4723f60651331047b0a32019781271`;
-
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      console.log(data.results);
-      setMooveez(data.results);
-    } catch (error) {
-      console.log("error :>> ", error);
-    }
-  };
-
+  const { mooveez, getMooveez } = useContext(MooveezContext);
   useEffect(() => {
     getMooveez();
   }, []);
@@ -27,7 +14,11 @@ function Mooveez() {
     <div className="homeDiv bg-neutral-900 text-neutral-50">
       <HeadingPopup />
       <div>
-        <input className="text-neutral-900 rounded mb-1 mt-3 w-80 p-1 border-2 border-red-600" type="search" placeholder="Search the latest mooveez..."></input>
+        <input
+          className="text-neutral-900 rounded mb-1 mt-3 w-80 p-1 border-2 border-red-600"
+          type="search"
+          placeholder="Search the latest mooveez..."
+        ></input>
       </div>
       <div className="grid grid-cols-2 gap-6 pt-1 m-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mooveeCardGrid">
         {mooveez.map((moovee) => (
