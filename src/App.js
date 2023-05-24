e8cbe9f05034aa47297a0284c2825f8dd374ae74
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-24 17:07:48 +0200

    imports added and paths ammended

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import './views/Page404'
-import Mooveez from "./views/Mooveez"
-import About from './views/About';
-import Page404 from './views/Page404';
-import Navigation from './components/Navigation';
-import LoginRegister from './views/LoginRegister';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { app } from "./config/firebaseConfig";
+import Mooveez from "./views/Mooveez";
+import Favourites from './views/Favourites'
+import Login from "./views/Login";
+import Register from "./views/Register";
+import About from "./views/About";
+import Page404 from "./views/Page404";
+import Navigation from "./components/Navigation";
+
+import { MooveezContextProvider } from "./context/MooveezContext";
+import { AuthContextProvider } from "./context/AuthContext";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <Navigation/>
-      <Routes>
-        <Route path="/" element={<Mooveez />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/login/register" element={<LoginRegister />} />
-        <Route path="*" element={<Page404 />} />
-      </Routes>
-
-
+    <div className="App bg-neutral-900 text-center sm:text-center">
+      <AuthContextProvider>
+        <Navigation />
+        <MooveezContextProvider>
+          <Routes>
+            <Route path="/" element={<Mooveez />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/favourites" element={<Favourites />} />
+            <Route path="*" element={<Page404 />} />
+          </Routes>
+        </MooveezContextProvider>
+      </AuthContextProvider>
     </div>
   );
 }
