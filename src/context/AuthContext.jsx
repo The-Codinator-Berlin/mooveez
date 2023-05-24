ec1d60147f709e133c085cb4b2257eae783ee183
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-24 20:05:14 +0200

    createUserWithEmailAndPassword function

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,12 +1,34 @@
 import { createContext, useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../config/FirebaseConfig.js";
 
 export const AuthContext = createContext();
 
 export const AuthContextProvider = (props) => {
   const [user, setUser] = useState(null);
+
+  const registerUser = async (email, password) => {
+    // console.log('email, password :>> ', email, password);
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log('userCrential :>> ', userCredential);
+      const user = userCredential.user;
+      setUser(user)
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log("errorMessage :>> ", errorMessage);
+    }
+  };
+
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
+    <AuthContext.Provider value={{ user, setUser, registerUser }}>
       {props.children}
     </AuthContext.Provider>
   );
 };
+  
