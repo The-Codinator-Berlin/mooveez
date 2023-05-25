02d047515cda557af8e701b955ab1ea493c50fba
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-25 15:26:04 +0200

    logout fully functional

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,15 +3,13 @@ import { Link } from "react-router-dom";
 import "../css/Navigation.css";
 import { AuthContext } from "../context/AuthContext";
 
-
 function Navigation() {
-  
-  const { user, setUser } = useContext(AuthContext);
+  const { user, logout } = useContext(AuthContext);
 
-  const logout = () => {
-    setUser(null);
+  const handleLogoutUser = () => {
+    logout();
   };
-  
+
   return (
     <>
       <nav className="navigation-box border-b	border-blue-700 h-10 bg-blue-950 p-2">
@@ -29,8 +27,8 @@ function Navigation() {
         </Link>
 
         {/* <span> */}
-          {/* {" "} */}
-          {/* {user ? (
+        {/* {" "} */}
+        {/* {user ? (
             <h5 className="text-green-500">{user.name} is logged in! </h5>
           ) : (
             <h2 className=" py-4 text-blue-600">
@@ -39,10 +37,32 @@ function Navigation() {
           )} */}
         {/* </span> */}
 
-        { user ? <><h5 className="text-green-500">{user.email} is logged in! </h5><Link to="/favourites" className="text-neutral-50 border-blue-700 rounded bg-green-600 hover:active:bg-red-500 hover:bg-slate-400 favouritesButton">Favourites</Link><Link onClick={logout}to="/" className="text-neutral-50 border-blue-700 rounded bg-red-500 hover:active:bg-red-500 hover:bg-slate-400 logOutButton">Log out  </Link></> : 
-        
-        <Link to="/login" className=" bg-blue-700	rounded text-neutral-50 navigation-link-login hover:active:bg-green-600 hover:bg-slate-400 loginButton border-blue-700 ">Login/Register</Link> }
-       
+        {user ? (
+          <>
+            <h5 className="text-green-500">{user.email} is logged in! </h5>
+            <Link
+              to="/favourites"
+              className="text-neutral-50 border-blue-700 rounded bg-green-600 hover:active:bg-red-500 hover:bg-slate-400 favouritesButton"
+            >
+              Favourites
+            </Link>
+            <Link
+              onClick={handleLogoutUser}
+              to="/"
+              className="text-neutral-50 border-blue-700 rounded bg-red-500 hover:active:bg-red-500 hover:bg-slate-400 logOutButton"
+            >
+              Log out{" "}
+            </Link>
+          </>
+        ) : (
+          <Link
+            to="/login"
+            className=" bg-blue-700	rounded text-neutral-50 navigation-link-login hover:active:bg-green-600 hover:bg-slate-400 loginButton border-blue-700 "
+          >
+            Login/Register
+          </Link>
+        )}
+
         {/* <Link
          to="/login/register"
           className="bg-red-500	rounded text-neutral-50 navigation-link-login hover:active:bg-green-600 hover:bg-slate-400 loginButton "
