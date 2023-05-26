507bed028a8e454745536ee38224d546b60fbf23
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-26 14:27:50 +0200

    logic added and nav to login

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,12 +1,15 @@
 import React, { useContext } from 'react'
 import { AuthContext } from '../context/AuthContext'
+import { Navigate } from 'react-router-dom';
 
 function ProtectedRoute(props) {
-
+console.log('props :>> ', props);
     const {user} =useContext(AuthContext)
+
+    const isUserAuth = user !==null ? true : false
   return (
-    <div>ProtectedRoute</div>
+    <>{isUserAuth ? props.children : <Navigate to="/login"/>}</>
   )
 }
 
-export default ProtectedRoute
+export default ProtectedRoute;
