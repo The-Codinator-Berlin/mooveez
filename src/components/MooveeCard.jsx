f878aa08e7880b7a5911d436bd07dfd454937589
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-29 17:35:00 +0200

    Updated component/ navigates to specific moovee/id

--- src/components/MooveeCard.jsx
+++ src/components/MooveeCard.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function MoveeCard({ moovee }) {
   const imgPath = "https://image.tmdb.org/t/p/original";
+  const toSingleMooveePage = useNavigate();
+  const handleNavigatetoSingleMooveePage = (e) => {
+    console.log(e.target.value);
+
+    toSingleMooveePage(`/movies/${moovee.id}`);  };
 
   return (
     <div>
@@ -18,11 +24,12 @@ function MoveeCard({ moovee }) {
         <h5>{moovee.title}</h5>
       </div>
       <button
-        type="button"
-        className="text-neutral-50 rounded-full px-2.5 py-1 text-xs font-semibold shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 text-neutral-50 hover:text-red-600 shadow-slate-50 shadow-md"
-      >
-        More +
-      </button>
+  onClick={handleNavigatetoSingleMooveePage}
+  type="button"
+  className="text-neutral-50 rounded-full px-2.5 py-1 text-xs font-semibold shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 text-neutral-50 hover:text-red-600 shadow-slate-50 shadow-md"
+>
+  More +
+</button>
     </div>
   );
 }
