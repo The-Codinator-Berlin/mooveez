c8567a0c70b5eaa319cd342d2952c5e911338776
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-29 20:58:22 +0200

    Commented out google profile pic/further dev

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -40,7 +40,16 @@ function Navigation() {
         {user ? (
           <>
             <h5 className="text-green-500">{user.email} is logged in! </h5>
-            <span><img className="profilePic" src={localStorage.getItem("profilePic")}/></span>
+            {/*//NOTE -  Google profile pic/ Still showing when logged out/ Leaving for further development */}
+            {/* {user && (
+              <span>
+                <img
+                  className="profilePic"
+                  src={localStorage.getItem("profilePic")}
+                  alt="Profile"
+                />
+              </span>
+            )} */}
             <Link
               to="/favourites"
               className="text-neutral-50 border-blue-700 rounded bg-green-600 hover:active:bg-red-500 hover:bg-slate-400 favouritesButton"
