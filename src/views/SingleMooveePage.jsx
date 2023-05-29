67e10cd73fcfc64f3149085eb0a9c1f915117c60
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-29 21:30:36 +0200

    SingleMooveePage added

---
+++ src/views/SingleMooveePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SingleMooveeContent from "../components/SingleMooveeContent";
+
+function SingleMooveePage({ mooveez }) {
+  return (
+    <div>
+      <SingleMooveeContent />
+    </div>
+  );
+}
+
+export default SingleMooveePage;
