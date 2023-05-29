c6a0ce24f5fc822c02c259edb2b6c8ec9ebba692
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-29 21:01:30 +0200

    Google auth in env

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth,GoogleAuthProvider } from "firebase/auth";
 
 
 
@@ -16,3 +16,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
