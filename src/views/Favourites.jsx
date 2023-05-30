69887d13d6f62b3b2a7c33d10d9346a2ff3c01c5
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-30 05:58:02 +0200

    Added h1 & h3

--- src/views/Favourites.jsx
+++ src/views/Favourites.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
 function Favourites() {
   return (
-    <div>Favourites</div>
+    <div className='text-white'>
+      <div>
+      <h1 className="text-9xl text-red-600 about-h1 font-light">Favouites</h1>
+      <h3 className="text-red-600 font-light">Save the movies you love for later</h3>
+      </div>
+    </div>
+    
   )
 }
 
