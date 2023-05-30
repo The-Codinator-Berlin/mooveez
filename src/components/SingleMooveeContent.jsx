c8494a34bda830b3ef9c9bc19a1243fd579c3cd5
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-30 20:25:32 +0200

    Added firestore getDocs func & registeres in c.log

--- src/components/SingleMooveeContent.jsx
+++ src/components/SingleMooveeContent.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { collection, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { db } from "../config/FirebaseConfig";
+
 function SingleMooveePageCard({ moovee }) {
   const imgPath = "https://image.tmdb.org/t/p/original";
+  const [TextAreaInput, setTextAreaInput] = useState("");
 
+  const HandleTextAreaInput = (e) => {
+    console.log("e.target.value :>> ", e.target.value);
+    setTextAreaInput(e.target.value);
+  };
+
+  const [mooveeComments, setMooveeComments] = useState([])
+  
+  const  getMooveeComments = async () => {
+    const querySnapshot = await getDocs(collection(db, "Comment"));
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data()}`);
+    });
+    
+  }
+  useEffect(() => {
+    getMooveeComments();
+  }, [])
+  
   return (
     <div className="text-white font-light border-2 rounded border-blue-950">
       <div className="flex justify-around m-4">
@@ -26,16 +48,17 @@ function SingleMooveePageCard({ moovee }) {
           <div>
             <h6 className="font-light">Release date: {moovee.release_date}</h6>
             <div className="flex justify-center pt-3">
-            <h6 className="bg-green-500 w-12 rounded">{moovee.vote_average}</h6>
-
+              <h6 className="bg-green-500 w-12 rounded">
+                {moovee.vote_average}
+              </h6>
             </div>
           </div>
         </div>
       </div>
       <div className="flex-col h-80 w-100 displayComments py-6 bg-blue-600 text-black">
-          <h2 className="font-bold mb-3">Feel free to leave a comment!</h2>
-            <h4> Test hard coded comment 1</h4>
-            <h4>Test hard coded comment 2</h4>
+        <h2 className="font-bold mb-3">Feel free to leave a comment!</h2>
+        <h4> Test hard coded comment 1</h4>
+        <h4>Test hard coded comment 2</h4>
       </div>
       <div className="m-6 border-2 border-red-600 rounded w-100">
         <input
@@ -43,6 +66,8 @@ function SingleMooveePageCard({ moovee }) {
           type="textarea"
           name="textValue"
           id="text"
+          value={TextAreaInput}
+          onChange={HandleTextAreaInput}
           placeholder="Type your comment here and click the green button below to submit it!"
         />
       </div>
