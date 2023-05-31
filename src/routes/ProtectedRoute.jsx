30611a08b338b31f575963417247275fea991168
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-05-31 19:07:47 +0200

    Protected route logic updated

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,18 +1,25 @@
-import React, { useContext } from 'react'
-import { AuthContext } from '../context/AuthContext'
-import { Navigate } from 'react-router-dom';
+import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { Navigate } from "react-router-dom";
 
 function ProtectedRoute(props) {
-  const { user } = useContext(AuthContext)
-  const isUserAuth = user !== null
-  console.log('isUserAuth :>> ', isUserAuth);
+  const { user, loading } = useContext(AuthContext);
+  const isUserAuth = user !== null;
+  console.log("isUserAuth :>> ", isUserAuth);
 
-  if (!isUserAuth) {
-    alert('Please login to access features');
-    return <Navigate to="/login" />;
-  }
-
-  return <>{props.children}</>;
+  return (
+    <>
+      {loading ? (
+        <h1 style={{ color: "white" }}>...loading.....</h1>
+      ) : isUserAuth ? (
+        props.children
+      ) : (
+        <>
+          {alert("Please login to access features")}
+          <Navigate to="/login" />
+        </>
+      )}
+    </>
+  );
 }
-
 export default ProtectedRoute;
