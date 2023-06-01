37582928729e2d00e49ab410a696a15ee66812e0
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-01 22:39:38 +0200

    Comments added

--- src/components/HeadingPopUp2.jsx
+++ src/components/HeadingPopUp2.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
-import '../css/HeadingPopUp.css'
+import "../css/HeadingPopUp.css";
 
+//SECTION ------------------------------------------------------------------------------------------------------------------>
+//NOTE - This function populates h3 after 2.5 seconds on the Mooveez page
 
 function HeadingPopup2() {
   const [isOpeningScreenVisible2, setOpeningScreenVisible2] = useState(false);
@@ -10,9 +12,15 @@ function HeadingPopup2() {
 
   return (
     <div className="mooveezBox">
-      {isOpeningScreenVisible2 && <h3 className="text-red-600 font-light">Mooveez are for cows, not just for people!</h3>}
+      {isOpeningScreenVisible2 && (
+        <h3 className="text-red-600 font-light">
+          Mooveez are for cows, not just for people!
+        </h3>
+      )}
     </div>
   );
 }
+
+// ------------------------------------------------------------------------------------------------------------------------->
 
 export default HeadingPopup2;
