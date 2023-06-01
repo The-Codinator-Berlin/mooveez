b25a81eedfb7da7218e7bdba3ad64f7d6e9d899a
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-01 22:42:12 +0200

    Comments added

--- src/components/HeadingPopup.jsx
+++ src/components/HeadingPopup.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
-import '../css/HeadingPopUp.css'
+import "../css/HeadingPopUp.css";
 import HeadingPopup2 from "./HeadingPopUp2";
 
+//SECTION ------------------------------------------------------------------------------------------------------------------>
+//NOTE - This function populates h1 after 1 seconds on the Mooveez page
 
 function HeadingPopup() {
   const [isOpeningScreenVisible, setOpeningScreenVisible] = useState(false);
@@ -11,11 +13,14 @@ function HeadingPopup() {
 
   return (
     <div className="mooveezBox">
-      {isOpeningScreenVisible &&<h1 className="sm: text-4xl md:text-9xl text-red-600">MOOVEEZ
-      </h1>}
+      {isOpeningScreenVisible && (
+        <h1 className="sm: text-4xl md:text-9xl text-red-600">MOOVEEZ</h1>
+      )}
       <HeadingPopup2 />
     </div>
   );
 }
+
+//-------------------------------------------------------------------------------------------------------------------------->
 
 export default HeadingPopup;
