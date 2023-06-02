502ed27ac680ca21e84242d7f6225dfb4cf4298b
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-02 11:45:22 +0200

    Comments added

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useEffect, useState } from "react";
+import { auth, provider } from "../config/FirebaseConfig.js";
 import {
   createUserWithEmailAndPassword,
   onAuthStateChanged,
@@ -6,17 +7,26 @@ import {
   signInWithPopup,
   signOut,
 } from "firebase/auth";
-import { auth, provider } from "../config/FirebaseConfig.js";
 import { useNavigate } from "react-router-dom";
+
+//SECTION ------------------------------------------------------------------------------------------->
+//NOTE - creating the context and exporting it so that it is available anywhere throughout my app
 
 export const AuthContext = createContext();
 
-
+//SECTION - // -------------------------------------------------------------------------------------->
+//NOTE - Initial states relating to authentication set and saved in variables
 export const AuthContextProvider = (props) => {
   const navigateToMooveezOnLog = useNavigate();
   const [user, setUser] = useState(null);
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
 
+  // ------------------------------------------------------------------------------------------------>
+
+  //SECTION ----------------------------------------------------------------------------------------->
+  //NOTE - Firebase registerUser async function that takes email and password as paramand checks if the credentials
+  //are correct before completeing the process and navigating to Mooveez(home page)
   const registerUser = async (email, password) => {
     try {
       const userCredential = await createUserWithEmailAndPassword(
@@ -38,7 +48,12 @@ export const AuthContextProvider = (props) => {
     }
   };
 
-  const login = async (email, password) => {
+  // ------------------------------------------------------------------------------------------------>
+
+  //SECTION ----------------------------------------------------------------------------------------->
+  //NOTE - Firebase login async function that also takes email and password as paramand checks if the credentials
+  //are correct before completeing the process and navigating to Mooveez(home page)
+  const Login = async (email, password) => {
     try {
       const userCredential = await signInWithEmailAndPassword(
         auth,
@@ -52,7 +67,8 @@ export const AuthContextProvider = (props) => {
     } catch (error) {
       const errorCode = error.code;
       const errorMessage = error.message;
-      // TODO if lese handling of error to check if email is already used or not.
+      //NOTE - Leaving TODO for further development
+      // TODO if lese handling of error to check if email is already used or not
       // if(errorCode===400){
       //   setError("invalid email")
       // }
@@ -61,6 +77,13 @@ export const AuthContextProvider = (props) => {
     }
   };
 
+  // ------------------------------------------------------------------------------------------------>
+
+  //SECTION ----------------------------------------------------------------------------------------->
+  //NOTE - googleLogin function uses signInWithPopup async function from Firebase that perferoms the
+  //signInWithPopup function using the auth object and provider.It cecks if there is a name, email
+  //and profile picture and if there is stores them in local storage within the page and logs them
+  //in, then navigating to Mooveez(home page)
   const googleLogin = async () => {
     try {
       const result = await signInWithPopup(auth, provider);
@@ -77,6 +100,13 @@ export const AuthContextProvider = (props) => {
     }
   };
 
+  // ------------------------------------------------------------------------------------------------>
+
+  //SECTION ----------------------------------------------------------------------------------------->
+  //NOTE - CheckIfUserIsLoggedIn function uses a Firebase onAuthStateChanged function taking the
+  //auth(authentication process from Firebase) and the user and checks the state of the user and if
+  //they are logged or not
+
   const checkIfUserIsLoggedIn = () => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
@@ -85,16 +115,23 @@ export const AuthContextProvider = (props) => {
         // ...
         console.log("User IS logged in!", user);
         setUser(user);
+        setLoading(false);
       } else {
         // User is signed out
         // ...
         console.log("User is NOT loggin in!");
         setUser(null);
+        setLoading(false);
       }
     });
   };
 
-  const logout = async () => {
+  // ------------------------------------------------------------------------------------------------>
+
+  //SECTION ----------------------------------------------------------------------------------------->
+  //NOTE - Logout function is using an async function form Firebase signOut which checks auth and
+  //provider and if passes sets user to null, if not user is still active.
+  const Logout = async () => {
     try {
       await signOut(auth, provider);
       setUser(null);
@@ -105,21 +142,31 @@ export const AuthContextProvider = (props) => {
     }
   };
 
-  // const AccessMoreInfoForMooveeWhenLogged = () => {
-  //   if (user) {
-  //     MooveeCardMoreButton()
-  //   } else {
-  //     alert("Please login to access features!");
-  //   }
-  // };
+  // ------------------------------------------------------------------------------------------------>
 
+  //SECTION ----------------------------------------------------------------------------------------->
+  //NOTE - UseEffect function called every time page is loaded to check if user is logged in or not
   useEffect(() => {
     checkIfUserIsLoggedIn();
   }, []);
 
+  // ------------------------------------------------------------------------------------------------>
+
+  //SECTION ----------------------------------------------------------------------------------------->
+  //NOTE - This is where all the functions are exported and made available to the rest of the components/pages.
   return (
     <AuthContext.Provider
-      value={{ user, setUser, registerUser, login, logout, error, googleLogin }}
+      value={{
+        user,
+        setUser,
+        registerUser,
+        Login,
+        loading,
+        Logout,
+        error,
+        googleLogin,
+        checkIfUserIsLoggedIn,
+      }}
     >
       {props.children}
     </AuthContext.Provider>
