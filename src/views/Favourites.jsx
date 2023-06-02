4d4f9ad38ffe928a909acf6e1f2fedabd79bfef8
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-02 12:59:59 +0200

    Comments added

--- src/views/Favourites.jsx
+++ src/views/Favourites.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from "react";
+
+//SECTION ---------------------------------------------------------------------------->
+//NOTE - This function displays the headings on the Favouites page (protected route)
+
 function Favourites() {
   return (
-    <div className='text-white'>
+    <div className="text-white">
       <div>
-      <h1 className="text-9xl text-red-600 about-h1 font-light">Favouites</h1>
-      <h3 className="text-red-600 font-light">Save the movies you love for later</h3>
+        <h1 className="text-9xl text-red-600 about-h1 font-light">Favouites</h1>
+        <h3 className="text-red-600 font-light">
+          Save the movies you love for later
+        </h3>
       </div>
     </div>
-    
-  )
+  );
 }
+// ----------------------------------------------------------------------------------->
 
 export default Favourites;
