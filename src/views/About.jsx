7130158b1a108b89f5feaa6615a77ac68f518cda
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-02 12:56:23 +0200

    Comments added

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,22 +1,56 @@
 import React from "react";
-import '../css/About.css'
+import "../css/About.css";
+
+//SECTION ------------------------------------------------------------------------------------------------------------------------------------------->
+//NOTE - This functiom displays the content for the About page
+
 function About() {
   return (
     <div>
       <div>
-      <h1 className="text-9xl text-red-600 about-h1 font-light">About</h1>
+        <h1 className="text-9xl text-red-600 about-h1 font-light">About</h1>
       </div>
       <span className="aboutDiv">
-      <section className="text-red-600 pt-14">This website is designed by Carl Manley.<br></br><a className="hover:text-slate-400" href="mailto:carl.manley.berlin@gmail.com"><span className="text-blue-700 hover:text-slate-400">carl.manley.berlin@gmail.com</span></a><br></br>GitHub: <a className="hover:text-slate-400" href="https://github.com/The-Codinator-Berlin"><span className="text-blue-700 hover:text-slate-400">https://github.com/The-Codinator-Berlin</span></a> <br></br><br></br><br></br>This product uses the <span className="text-blue-700 hover:text-slate-400"><a href="https://www.themoviedb.org/">TMDB API</a></span> but is not endorsed or certified by TMDB.
-      <br></br> <br></br>
-      <div className="tmdbLogoBox">
-      <img className="logo" src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg" alt="TMDB Logo"/>
-    </div>
+        <section className="text-red-600 pt-14">
+          This website is designed by Carl Manley.<br></br>
+          <a
+            className="hover:text-slate-400"
+            href="mailto:carl.manley.berlin@gmail.com"
+          >
+            <span className="text-blue-700 hover:text-slate-400">
+              carl.manley.berlin@gmail.com
+            </span>
+          </a>
+          <br></br>GitHub:{" "}
+          <a
+            className="hover:text-slate-400"
+            href="https://github.com/The-Codinator-Berlin"
+          >
+            <span className="text-blue-700 hover:text-slate-400">
+              https://github.com/The-Codinator-Berlin
+            </span>
+          </a>{" "}
           <br></br>
-      Built purely for educational purposes.</section>
-      
+          <br></br>
+          <br></br>This product uses the{" "}
+          <span className="text-blue-700 hover:text-slate-400">
+            <a href="https://www.themoviedb.org/">TMDB API</a>
+          </span>{" "}
+          but is not endorsed or certified by TMDB.
+          <br></br> <br></br>
+          <div className="tmdbLogoBox">
+            <img
+              className="logo"
+              src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg"
+              alt="TMDB Logo"
+            />
+          </div>
+          <br></br>
+          Built purely for educational purposes.
+        </section>
       </span>
-      </div>
+    </div>
+    //----------------------------------------------------------------------------------------------------------------------------------------------->
   );
 }
 
