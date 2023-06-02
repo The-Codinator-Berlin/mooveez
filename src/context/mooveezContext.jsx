ae04a93ae344a9c13969ad0fcd230bd2b804d031
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-02 12:52:09 +0200

    Comments added

--- src/context/mooveezContext.jsx
+++ src/context/mooveezContext.jsx
@@ -1,16 +1,30 @@
 import { createContext, useState } from "react";
 
+//SECTION ------------------------------------------------------------------------------------------------->
+//NOTE - MooveezContext created and exported
+
 export const MooveezContext = createContext();
 
-export const MooveezContextProvider = (props) => {
-  // console.log('props :>> ', props);
+// -------------------------------------------------------------------------------------------------------->
 
+//SECTION ------------------------------------------------------------------------------------------------->
+//NOTE - MooveezContextProvider function created and exported, setting the state of the array to empty which
+//will allow any moovees that the API is giving us to be stored
+
+export const MooveezContextProvider = (props) => {
   const [mooveez, setMooveez] = useState([]);
 
+  //SECTION ------------------------------------------------------------------------------------------------->
+  //NOTE - geMooveez function is fetching the data from the API
 
   const getMooveez = async () => {
+    //NOTE - Here the endpoint is stored in a variable URL and the API key from the .env file is imported
+    //into the URL
     const url = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TBBD_API_KEY}`;
-
+    //NOTE - The function then tries to fetch the data from the API and if there is a response it stores the information
+    //into JSON format making it available for use. Mooveez is then being set (setMooveez) with the data
+    //storing it in the variable, allowing us to use this across each file when needed. If there is no response
+    //then with the try/catch it will catch the error.
     try {
       const response = await fetch(url);
       const data = await response.json();
@@ -21,8 +35,13 @@ export const MooveezContextProvider = (props) => {
     }
   };
 
+  // -------------------------------------------------------------------------------------------------------->
+
+  //SECTION ------------------------------------------------------------------------------------------------->
+  //NOTE - This allows the routes that alre within the MooveezContext.Provider to have access to mooveez, getMooveez
+
   return (
-    <MooveezContext.Provider value={{ mooveez, getMooveez}}>
+    <MooveezContext.Provider value={{ mooveez, getMooveez }}>
       {props.children}
     </MooveezContext.Provider>
   );
