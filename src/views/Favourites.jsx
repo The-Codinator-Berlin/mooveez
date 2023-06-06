1de8dc1b1b13add137705d6f5907ea44d14f4fbc
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-06 08:57:04 +0200

    Typo fixed

--- src/views/Favourites.jsx
+++ src/views/Favourites.jsx
@@ -8,7 +8,9 @@ function Favourites() {
   return (
     <div className="text-white">
       <div>
-        <h1 className="text-9xl text-red-600 about-h1 font-light">Favouites</h1>
+        <h1 className="text-9xl text-red-600 about-h1 font-light">
+          Favourites
+        </h1>
         <h3 className="text-red-600 font-light">
           Save the movies you love for later
         </h3>
